6b8ec9f8465eaa0a5ce31fc8ffe82177aee9bd8b
Garglesoap <32884213+Garglesoap@users.noreply.github.com>
2018-08-16 03:08:19 -0400

    correctly prints models
    actually working caret model printer

--- Models/CaretList-splitter.R
+++ Models/CaretList-splitter.R
@@ -1,27 +1,28 @@
 
 #this splits caret model lists into singles and saves as RDS
 setwd('C:/Users/Po/Desktop/crypto data/8.11.2018')
-pairs <- c('ADABTC', 'BNBBTC', 'BNBUSDT', 'EOSBTC', 'ETHBTC', 'ETHUSDT', 'NEOBTC', 'TRXBTC', 'XLMBTC') 
+pairs <- c('ADABTC', 'BNBBTC', 'BNBUSDT', 'EOSBTC', 'ETHBTC', 'ETHUSDT', 'NEOBTC', 'TRXBTC', 'XLMBTC', 'BTCUSDT') 
 
+currentList <- list()
 for(pair in pairs){
   if(pair %in% c('BNBBTC','bnbbtc')){
     for(t in 1:3){
       nameE <- paste0("lists/A", t, "-", pair, "-modellist")
-      currentList <- readRDS(nameE)  
-      for(i in 1:6){
-        nameM <- paste0("A", t, "-", pair, '-', currentList[[i]]$method)
+      currentList[[t]] <- readRDS(nameE)  
+      for(m in 1:6){
+        nameM <- paste0("A", t, "-", pair, '-', currentList[[t]][[m]]$method)
         print(nameM)
-        saveRDS(currentList$models[[i]], nameM)
+        saveRDS(currentList[[t]][[m]], nameM)
       }
     }
   }else{
     for(t in 1:6){#read in 6 ensemble times
       nameE <- paste0("lists/A", t, "-", pair, "-modellist")
-      currentList <- readRDS(nameE)
-      for(i in 1:6){
-        nameM <- paste0("A", t, "-", pair, '-', currentList[[i]]$method)
+      currentList[[t]] <- readRDS(nameE)
+      for(m in 1:6){
+        nameM <- paste0("A", t, "-", pair, '-', currentList[[t]][[m]]$method)
         print(nameM)
-        saveRDS(currentList$models[[i]], nameM)
+        saveRDS(currentList[[t]][[m]], nameM)
       } 
     }
   }

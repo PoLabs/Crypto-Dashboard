26f4d5ac5639583d3406b50a5c87db285f39a546
Vincent Pisano <vincent.pisano1@gmail.com>
2018-12-21 22:58:35 -0500

    some pyth stuff

--- Models/Linear-model-ensemble.R
+++ Models/Linear-model-ensemble.R
@@ -3,14 +3,15 @@ pcklibs <- c("dplyr", "caTools", "Metrics", "rpart", "pROC", "caret", "caretEnse
 lapply(pcklibs, require, character.only=TRUE)
 set.seed(420420)
 # configure multicore
-# library(doMC)
-# registerDoMC(cores=16)
+ # library(doMC)
+ # registerDoMC(cores=7)
+
 # library(doParallel)
-# cl <- makePSOCKcluster(12)
+# cl <- makePSOCKcluster(7)
 # registerDoParallel(cl)
 
-pair.vector <- c('BTCUSDT')#, 'TRXBTC', 'ADABTC', 'EOSBTC', 'IOTABTC', 'NEOBTC', 'XLMBTC', 'ETHUSDT', 'BNBUSDT', 'BNBBTC', 'ETHBTC')
-#pair.vector <- c('XLMBTC', 'ETHUSDT', 'BNBUSDT', 'BNBBTC', 'ETHBTC', 'BCCBTC', 'ICXBTC', 'LTCBTC', 'NANOBTC', 'OMGBTC', 'ONTBTC', 'VENBTC', 'XMRBTC', 'XRPBTC', 'XEMBTC')
+pair.vector <- c('BTCUSDT')#, 'ADABTC', 'EOSBTC', 'IOTABTC', 'NEOBTC', 'XLMBTC', 'ETHUSDT', 'BNBUSDT', 'BNBBTC', 'ETHBTC')
+#pair.vector <- c('XLMBTC', 'ETHUSDT', 'BNBUSDT', 'BNBBTC', 'ETHBTC', 'BCCBTC', 'ICXBTC', 'LTCBTC', 'NANOBTC', 'OMGBTC', 'ONTBTC', 'VENBTC', 'XMRBTC', 'XRPBTC', 'XEMBTC')'TRXBTC',
 # pair.vector <- c('BTCUSDT')
 
 tic("start time")
@@ -24,37 +25,39 @@ for(pair in pair.vector){
   dataset.list[[5]] <- fread(file=paste0(pair, '-a5.csv'))
   dataset.list[[6]] <- fread(file=paste0(pair, '-a6.csv'))
   
-  for (i in 2:6){
+  for (i in 1:6){
     print(paste0(pair, ' frame # ', i))
     #model: we now have all 6 Y's in combined dfs for BTC
     ###################
     #XGBoost tune grid
-    xgb_grid_A1 = expand.grid(nrounds = c(1000), eta = c(0.3519),
-                              max_depth = c(6), subsample = c(0.702977), gamma = c(5.07345), colsample_bytree = c(0.43328), min_child_weight = c(17) )
-    
-    #################
-    #SVM-RBF tuner
-    svmrbf_grid_A1 = expand.grid(C=c(1.40537), sigma=c(0.13114)) #method = 'svmRadial'
-    
-    ###################
-    #random forest tune grid
-    rf_grid_A1 = expand.grid(mtry = c(10))
-    
+    # xgb_grid_A1 = expand.grid(nrounds = c(1000), eta = c(0.3519),
+    #                           max_depth = c(6), subsample = c(0.702977), gamma = c(5.07345), colsample_bytree = c(0.43328), min_child_weight = c(17) )
+    # 
+    # #################
+    # #SVM-RBF tuner
+    # svmrbf_grid_A1 = expand.grid(C=c(1.40537), sigma=c(0.13114)) #method = 'svmRadial'
+    # 
+    # ###################
+    # #random forest tune grid
+    # rf_grid_A1 = expand.grid(mtry = c(10))
+
     #Ensemble
-    Ensemble_control_A1 <- trainControl(method = "repeatedcv",  number = 15,  repeats = 2,  verboseIter = TRUE,  returnData = FALSE, trim=TRUE, returnResamp = "all", 
-                                        classProbs = TRUE, summaryFunction = twoClassSummary,  savePredictions = TRUE,  allowParallel = TRUE,  sampling = "up")
-    
+    Ensemble_control_A1 <- trainControl(method = "repeatedcv",  number = 10,  repeats = 2,  verboseIter = TRUE,  returnData = FALSE, trim=TRUE, returnResamp = "all", search = "random", #or grid
+                                        classProbs = TRUE, summaryFunction = twoClassSummary, savePredictions = TRUE,  allowParallel = TRUE,  sampling = "up")
+
     yE = dataset.list[[i]][,1]
     xE = data.matrix(dataset.list[[i]][,-1])
     yEf <- yE
     yEf <- ifelse(yE == 0, "no", "yes") 
     yEf <- factor(yEf)
     
-    Ensemble_list_A1 <- caretList(preProcess=c("center", "scale"),  x=xE,  y=yEf,  trControl=Ensemble_control_A1,  metric="ROC",  methodList=c("glm", "rpart", "bayesglm"),
-                                  tuneList=list(xgbA1=caretModelSpec(method="xgbTree", tuneGrid=xgb_grid_A1 ), svmA1=caretModelSpec(method="svmRadialSigma", tuneGrid=svmrbf_grid_A1 ),  #       
-                                                rfA1=caretModelSpec(method="rf", tuneGrid=rf_grid_A1) )) #)))#
+    Ensemble_list_A1 <- caretList(preProcess=c("center", "scale"),  x=xE,  y=yEf,  trControl=Ensemble_control_A1,  metric="ROC", methodList=c("glm", "rpart", "bayesglm"), 
+                                  tuneList=list(
+                                    xgbA1=caretModelSpec(method="xgbTree"), #nrounds = c(200)),#, tuneGrid=xgb_grid_A1 ), 
+                                    svmA1=caretModelSpec(method="svmRadialSigma"),#, tuneGrid=svmrbf_grid_A1 ),
+                                    rfA1=caretModelSpec(method="rf")))#, tuneGrid=rf_grid_A1) )) #)))#
     
-    Ensemble_greedy_A1 <- caretEnsemble(Ensemble_list_A1, metric="ROC", trControl=trainControl(number=5, summaryFunction=twoClassSummary, trim=TRUE, returnData=FALSE, classProbs=TRUE ))
+    Ensemble_greedy_A1 <- caretEnsemble(Ensemble_list_A1, metric="ROC", trControl=trainControl(number=5, summaryFunction=twoClassSummary, trim=TRUE, returnData=FALSE, classProbs=TRUE ), tuneLength = 100)
     summary(Ensemble_greedy_A1)
     
     CaretListName <- paste0('A', i, '-',pair,'-modellist')
